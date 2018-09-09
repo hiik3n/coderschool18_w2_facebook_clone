fc581937ae8b268d15c2904d08cda0ace8f8bd7d
Lam Do <hiik3n@gmail.com>
2018-09-09 16:18:43 +0700

    add home and user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+	before_action :sanitize_devise_params, if: :devise_controller?
+
+	def sanitize_devise_params
+   		devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:image_url,:bio])
+ 	end
 end
